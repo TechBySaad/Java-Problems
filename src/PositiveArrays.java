d69d1696d6e325d52b4221ea53eaf468d8a0e326
SaadDev <saad26dev@gmail.com>
2026-06-11 15:40:22 +0530

    Positive numbers in an array

--- src/PositiveArrays.java
+++ src/PositiveArrays.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
-public class OddArrays {
+public class PositiveArrays {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        System.out.print("Enter the size of Array: ");
+        System.out.print("Enter the Size of Array: ");
         int size = scanner.nextInt();
 
         int[] numbers = new int[size];
 
-        for (int i = 0; i < size; i++) {
-            System.out.print("Enter the #"+(i+1)+" number: ");
+        for (int i = 0; i < size ; i++) {
+            System.out.print("Enter the #" + (i+1)+ " number: ");
             numbers[i] = scanner.nextInt();
         }
-
-
         int count = 0;
-
         for (int i = 0; i < size; i++) {
-            if (numbers[i]%2 != 0) {
-
+            if(numbers[i]>0){
                 count++;
             }
+
         }
-        System.out.println(count);
+        System.out.println("Positive numbers count: "+ count);
 
 
-    }
-}
+    }}
+
